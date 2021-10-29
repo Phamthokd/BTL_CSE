064fb705441f3d03598610899da2760b7799d5f4
TranVanSinh1 <91180924+TranVanSinh1@users.noreply.github.com>
2021-10-29 00:05:32 -0700

    Add files via upload

---
+++ xoa-user.php
@@ -0,0 +1,17 @@
+<?php
+    $conn = mysqli_connect('localhost','root','','btl_ql');
+    if(!$conn){
+        die("Không thể kết nối,kiểm tra lại các tham số kết nối");
+    }
+    $id=$_GET['infor_id'];
+    $sql = "DELETE FROM infor_users WHERE infor_id=$id";
+    $res = mysqli_query($conn, $sql);
+    if($res==TRUE){
+        $value='xoa';
+        header("Location:index-user.php?response=$value");
+    }else{
+        echo 'xoa  tb';
+    }
+    mysqli_close($conn);
+
+?>
